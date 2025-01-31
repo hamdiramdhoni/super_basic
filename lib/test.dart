2b66717ef9e1d3f66c369780514fcc0c58e86574
hamdiramdhoni <mohamadhamdiramdhoni98@gmail.com>
2025-01-31 20:22:52 +0700

    add init state

---
+++ lib/test.dart
@@ -0,0 +1,43 @@
+// default constructor
+// class MyClass {
+//   MyClass() {
+//     print("default constructor");
+//   }
+// }
+
+// var myClass = MyClass();
+// constructor with parameters
+// class MyClass {
+//   int a;
+//   MyClass(int a) {
+//     this.a = a;
+//   }
+// }
+
+// var myClass = MyClass(10);
+// named constructor
+// class MyClass {
+//   int a;
+//   MyClass.namedConstructor(this.a);
+// }
+
+// var myClass = MyClass.namedConstructor(10);
+
+// contrants constructor
+// class MyClass {
+//   final int a;
+//   const MyClass(this.a);
+// }
+
+// var myClass = const MyClass(10);
+// factory constructor 
+// class MyClass {
+//   final int a;
+//   const MyClass(this.a);
+
+//   factory MyClass.create(int a) {
+//     return MyClass(a);
+//   }
+// }
+
+// var myClass = MyClass.create(10);
---
+++ lib/widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class MyStatefull extends StatefulWidget {
+  const MyStatefull({super.key});
+
+  @override
+  State<MyStatefull> createState() => _MyStatefullState();
+}
+
+class _MyStatefullState extends State<MyStatefull> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
